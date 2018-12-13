b830d66423db76dddb166db80446c3e594b83e11
avid-modal-4107 <norbertl@buffalo.edu>
2018-12-13 21:56:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  arr = []
+end
+
+def array_with_two_elements
+  [1, 2]
+end
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[3]
+end
+  
+def last_element(arr)
+  arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  arr.unshift
+
+
